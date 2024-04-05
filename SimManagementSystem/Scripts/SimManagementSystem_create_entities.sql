3c6d16708f07350663acc2aa85bdb9ad59b6aa79
s22773 <s22773@pjwstk.edu.pl>
2024-04-05 16:31:31 +0200

    Add DB scripts.

---
+++ SimManagementSystem/Scripts/SimManagementSystem_create_entities.sql
@@ -0,0 +1,236 @@
+CREATE TABLE Device (
+    ID int identity(1,1)  NOT NULL,
+    Name nvarchar(250)  NOT NULL,
+    Tag nvarchar(50)  NULL,
+    CONSTRAINT Device_pk PRIMARY KEY  (ID)
+);
+
+CREATE TABLE Inspection (
+    ID int identity(1,1)  NOT NULL,
+    Inspection_Type_ID int  NOT NULL,
+    Date datetime  NOT NULL,
+    Operator int  NOT NULL,
+    CONSTRAINT Inspection_pk PRIMARY KEY  (ID)
+);
+
+CREATE TABLE Inspection_Type (
+    ID int identity(1,1)  NOT NULL,
+    Name varchar(80)  NOT NULL,
+    CONSTRAINT Inspection_Type_pk PRIMARY KEY  (ID)
+);
+
+CREATE TABLE Maintenance (
+    ID int identity(1,1)  NOT NULL,
+    Type int  NOT NULL,
+    Executor int  NOT NULL,
+    Date datetime  NOT NULL,
+    Realized bit  NOT NULL,
+    CONSTRAINT Maintenance_pk PRIMARY KEY  (ID)
+);
+
+CREATE TABLE Maintenance_Type (
+    ID int identity(1,1)  NOT NULL,
+    Name varchar(64)  NOT NULL,
+    Tasks varchar(2500)  NOT NULL,
+    CONSTRAINT Maintenance_Type_pk PRIMARY KEY  (ID)
+);
+
+CREATE TABLE Malfunction (
+    ID int identity(1,1)  NOT NULL,
+    Name nvarchar(180)  NOT NULL,
+    Description nvarchar(2500)  NOT NULL,
+    User_Reporter int  NOT NULL,
+    User_Handler int  NOT NULL,
+    Date_Begin datetime  NOT NULL,
+    Date_End datetime  NULL,
+    Status bit  NOT NULL,
+    CONSTRAINT Malfunction_pk PRIMARY KEY  (ID)
+);
+
+CREATE TABLE MalfunctionDevice (
+    Device_ID int  NOT NULL,
+    Malfunction_ID int  NOT NULL,
+    CONSTRAINT MalfunctionDevice_pk PRIMARY KEY  (Device_ID,Malfunction_ID)
+);
+
+CREATE TABLE Predefined_Session (
+    ID int identity(1,1) NOT NULL,
+    Category int  NOT NULL,
+    Name nvarchar(240)  NOT NULL,
+    Description nvarchar(600)  NOT NULL,
+    Duration int  NOT NULL,
+    Abbreviation nvarchar(15)  NOT NULL,
+    CONSTRAINT Predefined_Session_pk PRIMARY KEY  (ID)
+);
+
+CREATE TABLE RecoveryAction (
+    ID int identity(1,1) NOT NULL,
+    Date datetime  NOT NULL,
+    Description nvarchar(2500)  NOT NULL,
+    Malfunction_ID int  NOT NULL,
+    CONSTRAINT RecoveryAction_pk PRIMARY KEY  (ID)
+);
+
+CREATE TABLE Role (
+    ID int identity(1,1) NOT NULL,
+    Name varchar(80)  NOT NULL,
+    Access_Malfunctions bit  NOT NULL,
+    Access_Maintenances bit  NOT NULL,
+    Access_Meter bit  NOT NULL,
+    Access_Inspection bit  NOT NULL,
+    Access_QTG bit  NOT NULL,
+    Access_Trainees bit  NOT NULL,
+    Access_Sessions bit  NOT NULL,
+    Access_Raports bit  NOT NULL,
+    CONSTRAINT Role_pk PRIMARY KEY  (ID)
+);
+
+CREATE TABLE Session_Category (
+    ID int identity(1,1) NOT NULL,
+    Name nvarchar(200)  NOT NULL,
+    CONSTRAINT Session_Category_pk PRIMARY KEY  (ID)
+);
+
+CREATE TABLE Simulator_Session (
+    ID int identity(1,1) NOT NULL,
+    Predefined_Session int  NOT NULL,
+    Date datetime  NOT NULL,
+    Pilot_Seat int  NULL,
+    Copilot_Seat int  NULL,
+    Supervisor_Seat int  NULL,
+    Observer_Seat int  NULL,
+    Realized bit  NOT NULL,
+    CONSTRAINT Simulator_Session_pk PRIMARY KEY  (ID)
+);
+
+CREATE TABLE Simulator_State (
+    ID int identity(1,1) NOT NULL,
+    Startup_Time datetime  NOT NULL,
+    Meter_State int  NOT NULL,
+    Operator int  NOT NULL,
+    CONSTRAINT Simulator_State_pk PRIMARY KEY  (ID)
+);
+
+CREATE TABLE "Statistics" (
+    ID int identity(1,1) NOT NULL,
+    Date_Begin datetime  NOT NULL,
+    Date_End datetime  NOT NULL,
+    Malfunctions_Count int  NOT NULL,
+    Maintenances_Count int  NOT NULL,
+    Sessions_Time int  NOT NULL,
+    Operating_Time int  NOT NULL,
+    Efficiency_Factor int  NOT NULL,
+    CONSTRAINT Statistics_pk PRIMARY KEY  (ID)
+);
+
+CREATE TABLE Test_QTG (
+    ID int identity(1,1) NOT NULL,
+    Stage varchar(20)  NOT NULL,
+    Title varchar(80)  NOT NULL,
+    Description varchar(2500)  NOT NULL,
+    CONSTRAINT Test_QTG_pk PRIMARY KEY  (ID)
+);
+
+CREATE TABLE Test_Result (
+    ID int identity(1,1) NOT NULL,
+    Test int  NOT NULL,
+    IsPassed bit  NOT NULL,
+    Observation varchar(1200)  NOT NULL,
+    Excutor int  NOT NULL,
+    CONSTRAINT Test_Result_pk PRIMARY KEY  (ID)
+);
+
+CREATE TABLE "User" (
+    ID int identity(1,1)  NOT NULL,
+    FirstName varchar(40)  NOT NULL,
+    LastName varchar(60)  NOT NULL,
+    Login nvarchar(64)  NOT NULL,
+    Password nvarchar(32)  NOT NULL,
+    CONSTRAINT User_pk PRIMARY KEY  (ID)
+);
+
+CREATE TABLE UserRole (
+    User_ID int  NOT NULL,
+    Role_ID int  NOT NULL,
+    CONSTRAINT UserRole_pk PRIMARY KEY  (User_ID,Role_ID)
+);
+
+ALTER TABLE Inspection ADD CONSTRAINT Inspection_Inspection_Type
+    FOREIGN KEY (Inspection_Type_ID)
+    REFERENCES Inspection_Type (ID);
+
+ALTER TABLE Inspection ADD CONSTRAINT Inspection_User
+    FOREIGN KEY (Operator)
+    REFERENCES "User" (ID);
+
+ALTER TABLE Maintenance ADD CONSTRAINT Maintenance_Maintenance_Type
+    FOREIGN KEY (Type)
+    REFERENCES Maintenance_Type (ID);
+
+ALTER TABLE Maintenance ADD CONSTRAINT Maintenance_User
+    FOREIGN KEY (Executor)
+    REFERENCES "User" (ID);
+
+ALTER TABLE MalfunctionDevice ADD CONSTRAINT MalfunctionDevice_Device
+    FOREIGN KEY (Device_ID)
+    REFERENCES Device (ID);
+
+ALTER TABLE MalfunctionDevice ADD CONSTRAINT MalfunctionDevice_Malfunction
+    FOREIGN KEY (Malfunction_ID)
+    REFERENCES Malfunction (ID);
+
+ALTER TABLE Malfunction ADD CONSTRAINT Malfunction_Handler
+    FOREIGN KEY (User_Handler)
+    REFERENCES "User" (ID);
+
+ALTER TABLE Malfunction ADD CONSTRAINT Malfunction_Reporter
+    FOREIGN KEY (User_Reporter)
+    REFERENCES "User" (ID);
+
+ALTER TABLE Predefined_Session ADD CONSTRAINT Predefined_Session_SessionCategory
+    FOREIGN KEY (Category)
+    REFERENCES Session_Category (ID);
+
+ALTER TABLE RecoveryAction ADD CONSTRAINT RecoveryAction_Malfunction
+    FOREIGN KEY (Malfunction_ID)
+    REFERENCES Malfunction (ID);
+
+ALTER TABLE Simulator_Session ADD CONSTRAINT SimulatorSession_Copilot
+    FOREIGN KEY (Observer_Seat)
+    REFERENCES "User" (ID);
+
+ALTER TABLE Simulator_Session ADD CONSTRAINT SimulatorSession_Observer
+    FOREIGN KEY (Supervisor_Seat)
+    REFERENCES "User" (ID);
+
+ALTER TABLE Simulator_Session ADD CONSTRAINT SimulatorSession_Pilot
+    FOREIGN KEY (Copilot_Seat)
+    REFERENCES "User" (ID);
+
+ALTER TABLE Simulator_Session ADD CONSTRAINT SimulatorSession_Supervisor
+    FOREIGN KEY (Pilot_Seat)
+    REFERENCES "User" (ID);
+
+ALTER TABLE Simulator_Session ADD CONSTRAINT Simulator_Session_Predefined_Session
+    FOREIGN KEY (Predefined_Session)
+    REFERENCES Predefined_Session (ID);
+
+ALTER TABLE Simulator_State ADD CONSTRAINT Simulator_State_User
+    FOREIGN KEY (Operator)
+    REFERENCES "User" (ID);
+
+ALTER TABLE Test_Result ADD CONSTRAINT Test_Result_Test_QTG
+    FOREIGN KEY (Test)
+    REFERENCES Test_QTG (ID);
+
+ALTER TABLE Test_Result ADD CONSTRAINT Test_Result_User
+    FOREIGN KEY (Excutor)
+    REFERENCES "User" (ID);
+
+ALTER TABLE UserRole ADD CONSTRAINT UserRole_Role
+    FOREIGN KEY (Role_ID)
+    REFERENCES Role (ID);
+
+ALTER TABLE UserRole ADD CONSTRAINT UserRole_User
+    FOREIGN KEY (User_ID)
+    REFERENCES "User" (ID);
---
+++ SimManagementSystem/Scripts/SimManagementSystem_seeding.sql
@@ -0,0 +1,103 @@
+INSERT INTO "Device" (Name, Tag) VALUES
+('Control loading system', 'CLS'),
+('Collective', 'COLLECTIVE'),
+('Autopilot', 'AP'),
+('Flight Management System', 'FMS'),
+('Instructor Operating Station', 'IOS'),
+('Clock', 'CLOCK'),
+('Projector', 'PROJECTOR');
+
+INSERT INTO "Inspection_Type" (Name) VALUES
+('Przegląd klimatyzacji'),
+('Wymiana filtra klimatyzacji'),
+('Sprawdzenie czujek przeciwpożarowych');
+
+INSERT INTO "User" (FirstName, LastName, Login, Password) VALUES
+('Andreas', 'Duo', 'andreas_duo', 'Andreas@123'),
+('Jude', 'Smiths', 'jude_smiths', 'Smiths@111Jude'),
+('Robert', 'Johanson', 'robert_johanson', 'pass!woRd331'),
+('Jakub', 'Świderski', 'jswiderski', 'jswiderski1'),
+('Sam', 'Kramer', 's.kramer', '9qWz&2mE$jP'),
+('Mikey', 'Walter', 'm.walter', '5@Kc!pF9z#Jx'),
+('Brendon', 'Rodrigueza', 'b.rodriguez11', 'H4@Ry&tN8qXu');
+
+INSERT INTO "Role" (Name, Access_Malfunctions, Access_Maintenances, Access_Meter, Access_Inspection, Access_QTG, Access_Trainees, Access_Sessions, Access_Raports) VALUES
+('Admin', 1, 1, 1, 1, 1, 1, 1, 1),
+('Engineer', 1, 1, 1, 1, 1, 1, 1, 1),
+('Planer', 0, 0, 0, 0, 0, 1, 1, 1),
+('Pilot', 0, 0, 0, 0, 0, 0, 1, 1),
+('Copilot', 0, 0, 0, 0, 0, 0, 1, 1),
+('Instructor', 1, 0, 0, 0, 0, 1, 1, 1),
+('Guest', 0, 0, 0, 0, 0, 0, 0, 0);
+
+INSERT INTO UserRole (User_ID, Role_ID) VALUES
+(1, 1),
+(2, 2),
+(3, 3),
+(4, 4),
+(5, 5),
+(6, 6),
+(7, 7);
+
+INSERT INTO Inspection (Inspection_Type_ID, Date, Operator) VALUES
+(1, '2024-04-01 08:00:00', 2),
+(2, '2024-04-02 09:30:00', 2),
+(3, '2024-04-03 10:45:00', 2);
+
+INSERT INTO Maintenance_Type (Name, Tasks) VALUES
+('Obsługa dzienna', 'Task1, Task2, Task3'),
+('Obsługa tygodniowa', 'Task4, Task5, Task6'),
+('Obsługa miesięczna', 'Task7, Task8, Task9');
+
+INSERT INTO Maintenance (Type, Executor, Date, Realized) VALUES
+(1, 2, '2024-04-05 08:00:00', 1),
+(2, 2, '2024-04-06 09:30:00', 1),
+(3, 2, '2024-04-07 10:45:00', 0);
+
+INSERT INTO Malfunction (Name, Description, User_Reporter, User_Handler, Date_Begin, Date_End, Status) VALUES
+('Brak podświetlenia zegarka', 'W trakcie misji tarcza zegarka przestała się świecić, w trakcie misji nocnej nie widać wskazania czasu.', 6, 2, '2024-04-01 08:00:00', NULL, 0),
+('Nie działa AP', 'Nie da się uruchomić procedury testowania AP', 6, 2, '2024-04-02 09:30:00', '2024-04-03 10:45:00', 0),
+('Brak obrazu', 'Nie działa wyświetlanie obrazu na trzecim od góry projektorze', 4, 2, '2024-04-03 10:45:00', NULL, 1);
+
+INSERT INTO MalfunctionDevice (Device_ID, Malfunction_ID) VALUES
+(6, 1),
+(3, 2),
+(7, 3);
+
+INSERT INTO Session_Category (Name) VALUES
+('Sesja treningowa'),
+('Sesja egzaminacyjna'),
+('Sesja obsługowa'),
+('Sesja pokazowa');
+
+INSERT INTO Predefined_Session (Category, Name, Description, Duration, Abbreviation) VALUES
+(1, 'Crew resource management', 'Sesja związana z zarządzaniem zasobami załogi.', 120, 'CRM'),
+(2, 'Operator Proficiency Check', 'Sesja oceny sprawności operatora.', 90, 'OPC');
+
+INSERT INTO RecoveryAction (Date, Description, Malfunction_ID) VALUES
+('2024-04-05 08:00:00', 'Próba przestawienia mechanizmu zegarka.', 1),
+('2024-04-06 09:30:00', 'Wymiana modułu podświetlenia zegarka', 1),
+('2024-04-07 10:45:00', 'Restart modułu Autopilota', 2);
+
+INSERT INTO Simulator_Session (Predefined_Session, Date, Pilot_Seat, Copilot_Seat, Supervisor_Seat, Observer_Seat, Realized) VALUES
+(1, '2024-04-01 08:00:00', 4, 5, 6, NULL, 1),
+(2, '2024-04-02 09:30:00', 4, NULL, 6, NULL, 1),
+(1, '2024-04-07 10:45:00', 4, 5, 6, NULL, 0);
+
+INSERT INTO Simulator_State (Startup_Time, Meter_State, Operator) VALUES
+('2024-04-01 08:00:00', 3221123, 2),
+('2024-04-02 09:30:00', 3222123, 2),
+('2024-04-03 10:45:00', 3223123, 2);
+
+INSERT INTO "Statistics" (Date_Begin, Date_End, Malfunctions_Count, Maintenances_Count, Sessions_Time, Operating_Time, Efficiency_Factor) VALUES
+('2024-04-01 00:00:00', '2024-04-01 23:59:59', 1, 2, 360, 480, 85);
+
+INSERT INTO Test_QTG (Stage, Title, Description) VALUES
+('Test1', 'Title1', 'Description1'),
+('Test2', 'Title2', 'Description2'),
+('Test3', 'Title3', 'Description3');
+
+INSERT INTO Test_Result (Test, IsPassed, Observation, Excutor) VALUES
+(1, 1, 'Passed', 2),
+(2, 0, 'Failed, too long opacity', 2),
+(3, 1, 'Passed', 2);
