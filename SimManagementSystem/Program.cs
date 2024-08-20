acdf494300aff591441df2127c6f773f5203db9a
jcobswiderski <jcobswiderski@gmail.com>
2024-08-20 15:34:57 +0200

    Restore local development properties

--- SimManagementSystem/Program.cs
+++ SimManagementSystem/Program.cs
@@ -52,14 +52,11 @@ var app = builder.Build();
 app.UseCors(c => c.AllowAnyHeader().AllowAnyOrigin().AllowAnyMethod());
 
 // Configure the HTTP request pipeline.
-//if (app.Environment.IsDevelopment())
-//{
+if (app.Environment.IsDevelopment())
+{
     app.UseSwagger();
-    app.UseSwaggerUI(c=> {
-        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Dispatch API V1");
-        c.RoutePrefix = string.Empty;
-    });
-//}
+    app.UseSwaggerUI();
+}
 
 app.UseAuthentication();
 app.UseAuthorization();
