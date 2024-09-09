7bebaada51e3aa7304ad9c424a6fb6da10271379
jcobswiderski <jcobswiderski@gmail.com>
2024-09-09 15:48:21 +0200

    Add simulator inspection endpoints

---
+++ SimManagementSystem/Controllers/InspectionsController.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SimManagementSystem.DataAccessLayer;
+using SimManagementSystem.DataTransferObjects;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace SimManagementSystem.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class InspectionsController : ControllerBase
+    {
+        private readonly SimManagementSystemContext _context;
+
+        public InspectionsController(SimManagementSystemContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult GetInspections()
+        {
+            var inspections = _context.Inspections
+                .Include(i => i.InspectionType)
+                .Select(i => new {
+                    i.Id,
+                    i.Date,
+                    InspectionType = i.InspectionType.Name,
+                    Operator = i.OperatorNavigation.FirstName + " " + i.OperatorNavigation.LastName
+                })
+                .ToList();
+            return Ok(inspections);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetInspection(int id)
+        {
+            var inspection = _context.Inspections
+                 .Include(i => i.InspectionType)
+                 .Where(i => i.InspectionTypeId == id)
+                 .Select(i => new {
+                     i.Id,
+                     i.Date,
+                     InspectionType = i.InspectionType.Name,
+                     Operator = i.OperatorNavigation.FirstName + " " + i.OperatorNavigation.LastName
+                 })
+                 .FirstOrDefault();
+
+            if (inspection == null)
+            {
+                return NotFound($"No inspection found for the id {id}.");
+            }
+
+
+            return Ok(inspection);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateInspection(CreateInspectionDTO newInspection)
+        {
+            var inspection = new Inspection
+            {
+                InspectionTypeId = newInspection.InspectionTypeId,
+                Date = newInspection.Date,
+                Operator = newInspection.Operator
+            };
+
+            await _context.Inspections.AddAsync(inspection);
+            await _context.SaveChangesAsync();
+
+            return Ok(inspection);
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> DeleteInspection(int id)
+        {
+            var inspectionToDelete = new Inspection
+            {
+                Id = id
+            };
+
+            _context.Inspections.Attach(inspectionToDelete);
+            _context.Inspections.Remove(inspectionToDelete);
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+    }
+}
---
+++ SimManagementSystem/DataTransferObjects/CreateInspectionDTO.cs
@@ -0,0 +1,13 @@
+﻿using SimManagementSystem.DataAccessLayer;
+
+namespace SimManagementSystem.DataTransferObjects
+{
+    public class CreateInspectionDTO
+    {
+        public int InspectionTypeId { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public int Operator { get; set; }
+    }
+}
